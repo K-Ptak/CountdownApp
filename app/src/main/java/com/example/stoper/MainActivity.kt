468a7c2b6ee1493d8936717848c7570fb1dddfe5
krzysztofterelak <k.terelak01@gmail.com>
2023-11-14 21:23:07 +0100

    initial setup

---
+++ app/src/main/java/com/example/stoper/MainActivity.kt
@@ -0,0 +1,171 @@
+package com.example.stoper
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private var seconds: Long = 0L
+    private var isRunning: Boolean = false
+
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.Default + job)
+    private lateinit var timer: Job
+
+    companion object {
+        private const val TIME_KEY = "time_Key"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        seconds = savedInstanceState?.getLong(TIME_KEY) ?: 0L
+        initView()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong(TIME_KEY, seconds)
+    }
+
+
+    private fun initView() {
+        val addMinute = findViewById<ImageButton>(R.id.minute_add)
+        val minusMinute = findViewById<ImageButton>(R.id.minute_minus)
+        val addSecond = findViewById<ImageButton>(R.id.second_add)
+        val minusSecond = findViewById<ImageButton>(R.id.second_minus)
+
+        val start = findViewById<Button>(R.id.start)
+        val pause = findViewById<Button>(R.id.pause)
+        val stop = findViewById<Button>(R.id.stop)
+
+        updateTimeOnView()
+
+        addMinute.setOnClickListener { if (!isRunning) addMinute() }
+        minusMinute.setOnClickListener { if (!isRunning) minusMinute() }
+
+        addSecond.setOnClickListener { if (!isRunning) addSecond() }
+        minusSecond.setOnClickListener { if (!isRunning) minusSecond() }
+
+        start.setOnClickListener {
+            if (isRunning) return@setOnClickListener
+            isRunning = true
+            updateButtonsOnView()
+            timer = startCountdown()
+            timer.start()
+        }
+
+        pause.setOnClickListener {
+            if (!isRunning) return@setOnClickListener
+            isRunning = false
+            updateButtonsOnView()
+            if (::timer.isInitialized) {
+                timer.cancel()
+            }
+        }
+
+        stop.setOnClickListener {
+            isRunning = false
+            updateButtonsOnView()
+            if (::timer.isInitialized) {
+                timer.cancel()
+            }
+            if (seconds != 0L) {
+                resetTime()
+            }
+        }
+    }
+
+    private fun startCountdown(): Job =
+        scope.launch(context = Dispatchers.IO, start = CoroutineStart.LAZY) {
+            while (isActive) {
+                if (seconds <= 0) {
+                    isRunning = false
+                    withContext(Main) {
+                        updateButtonsOnView()
+                        onCountdownFinish()
+                    }
+                    timer.cancel()
+                }
+                delay(1000)
+                withContext(Main) {
+                    minusSecond()
+                }
+            }
+        }
+
+    private fun updateTimeOnView() {
+        val minute1 = findViewById<TextView>(R.id.minute_1)
+        val minute2 = findViewById<TextView>(R.id.minute_2)
+        val second1 = findViewById<TextView>(R.id.second_1)
+        val second2 = findViewById<TextView>(R.id.second_2)
+
+        val (minutes, seconds) = formatTime(seconds)
+        minute1.text = minutes[0].toString()
+        minute2.text = minutes[1].toString()
+        second1.text = seconds[0].toString()
+        second2.text = seconds[1].toString()
+    }
+
+    private fun updateButtonsOnView() {
+        val start = findViewById<Button>(R.id.start)
+        val pause = findViewById<Button>(R.id.pause)
+
+        start.isEnabled = !isRunning
+        pause.isEnabled = isRunning
+    }
+
+    private fun resetTime() {
+        seconds = 0
+        updateTimeOnView()
+    }
+
+    private fun addMinute() {
+        if (seconds >= 60 * 60) return
+        seconds += 60
+        updateTimeOnView()
+    }
+
+    private fun minusMinute() {
+        if (seconds < 60) return
+        seconds -= 60
+        updateTimeOnView()
+    }
+
+    private fun addSecond() {
+        if (seconds > 60 * 60 + 59) return
+        seconds += 1
+        updateTimeOnView()
+    }
+
+    private fun minusSecond() {
+        if (seconds <= 0) return
+        seconds -= 1
+        updateTimeOnView()
+    }
+
+    private fun formatTime(time: Long): Pair<String, String> {
+        val minutes = time / 60
+        val remainingSeconds = time % 60
+
+        return Pair("%02d".format(minutes), "%02d".format(remainingSeconds))
+    }
+
+    private fun onCountdownFinish() {
+        Toast.makeText(this, "Finished!", Toast.LENGTH_SHORT).show()
+    }
+}
